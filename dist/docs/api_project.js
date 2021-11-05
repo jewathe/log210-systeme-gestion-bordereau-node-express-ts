d92487159afcfd26a41bfbc8c5c9201eadbeccda
jewathe <89857594+jewathe@users.noreply.github.com>
2021-11-05 10:47:03 -0400

    ssayer lister cours

--- dist/docs/api_project.js
+++ dist/docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-12T18:26:45.820Z",
+    "time": "2021-11-05T14:45:12.210Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
